fe7b63099483c7f11cc696eeeec9ce1bd3201ff4
Jack Gordon <127596954+JackGAttack4@users.noreply.github.com>
2023-03-20 18:46:10 +0000

    /maintaince command added

--- src/com/cashcraft/utils/MaintainceCommand.java
+++ src/com/cashcraft/utils/MaintainceCommand.java
@@ -1,5 +1,37 @@
 package com.cashcraft.utils;
 
-public class MaintainceCommand {
+import java.util.List;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+public class MaintainceCommand implements CommandExecutor {
+	@SuppressWarnings("unused")
+	private Main plugin;
+
+	public MaintainceCommand(Main plugin) {
+		this.plugin = plugin;
+		plugin.getCommand("maintaince").setExecutor(this);
+		plugin.getCommand("maintaince").setTabCompleter(new MaintainceTabComplete());
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		
+		
+		
+		
+		
+		return false;
+	}
+
+	private class MaintainceTabComplete implements TabCompleter {
+		public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+			return null;
+
+		}
+	}
 }
