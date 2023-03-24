c8711131c97090cc44d94a4459ead1e76aca17b4
Movies22 <111647781+Movs22@users.noreply.github.com>
2023-03-24 18:19:32 +0000

    Finished /metro and (un)mavenizing

--- src/com/cashcraft/utils/commands/MetroCommand.java
+++ src/com/cashcraft/utils/commands/MetroCommand.java
@@ -5,13 +5,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.block.CommandBlock;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import com.bergerkiller.bukkit.tc.pathfinding.PathNode;
+import com.bergerkiller.generated.net.minecraft.network.protocol.game.PacketPlayInTeleportAcceptHandle.PacketPlayInTeleportAcceptClass;
 import com.cashcraft.utils.Main;
 
 public class MetroCommand implements CommandExecutor {
@@ -39,32 +43,39 @@ public class MetroCommand implements CommandExecutor {
 			}
 			return true;
 		} else {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "You can't run this command as a " + (sender instanceof CommandBlock ? "Command Block" : "Server Console"));
+				return true;
+			}
+			Player p = (Player) sender;
 			List<String> s = (List<String>) plugin.getConfig().getList("Stations");
 			List<String> sc = (List<String>) plugin.getConfig().getList("StationCodes");
 			if(sc.contains(args[0])) {
-				String t;
+				String t = "a:INVALID";
 				if(args.length < 2) {
 					t = plugin.getConfig().getString(args[0] + ".Main");
 				} else {
 					t = plugin.getConfig().getString(args[0] + "." + String.join(" ", Arrays.asList(args).subList(1, args.length)));
 				}
-				plugin.getLogger().log(Level.INFO, args[0] + "." + String.join(" ", Arrays.asList(args).subList(1, args.length)));
-				plugin.getLogger().log(Level.INFO, "[Cashcraft Utils] (Test) Found station: " + t.toString() + " - " + (t.contains("t:") ? "[TC]" : "[Other node]")+ " for arg " + args[0]);
-				PathNode a = plugin.traincarts.getPathProvider().getWorld("Main1").getNodeByName(t.split("t:")[1]);
-				plugin.getLogger().log(Level.INFO, "[Cashcraf Utils] (Debug) Station " + t + " - Found node " + a.getName() + " - " + a.location + " for " + t);
-				if(t == null) {
-					sender.sendMessage(ChatColor.RED + "Station " + args[0] + " wasn't found.");
+				if(t.startsWith("t:")) {
+					PathNode a = plugin.traincarts.getPathProvider().getWorld("Main1").getNodeByName(t.split("t:")[1]);
+					Bukkit.dispatchCommand(sender, "tp " + sender.getName() + " " +  a.location.x + " " + a.location.y + " " + a.location.z);
+					sender.sendMessage(ChatColor.GREEN + "You've been teleported to " + ChatColor.GOLD +  args[0] + ":" + String.join(" ", Arrays.asList(args).subList(1, args.length)) + ChatColor.GREEN + ".");
 					return true;
 				}
-				if(t.contains("h:")) {
-					
-				} else if(t.contains("w:")) {
-					
-				} else if(t.contains("t:")) {
-					a = plugin.traincarts.getPathProvider().getWorld("Main1").getNodeByName(t.split("t:")[1]);
-					plugin.getLogger().log(Level.INFO, "[Cashcraf Utils] (Debug) Station " + t + " - Found node " + a.getName() + " - " + a.location + " for " + t);
+				if(t.startsWith("w:")) {
+					Bukkit.dispatchCommand(sender, "warp " + t.split("w:")[1]);
+					sender.sendMessage(ChatColor.GREEN + "You've been teleported to " + ChatColor.GOLD +  args[0] + ":" + String.join(" ", Arrays.asList(args).subList(1, args.length)) + ChatColor.GREEN + ".");
 					return true;
 				}
+				if(t.startsWith("h:")) {
+					Bukkit.dispatchCommand(sender, "home " + t.split("h:")[1]);
+					sender.sendMessage(ChatColor.GREEN + "You've been teleported to " + ChatColor.GOLD +  args[0] + ":" + String.join(" ", Arrays.asList(args).subList(1, args.length)) + ChatColor.GREEN + ".");
+					return true;
+				}
+				//plugin.getLogger().log(Level.INFO, "[Cashcraft Utils] (Test) Found station: " + t.toString() + " - " + (t.contains("t:") ? "[TC]" : "[Other node]")+ " for arg " + args[0]);
+				//plugin.getLogger().log(Level.INFO, "[Cashcraf Utils] (Debug) Station " + t + " - Found node " + a.getName() + " - " + a.location + " for " + t);
+				
 				
 			} else {
 				sender.sendMessage(ChatColor.RED + "Station " + args[0] + " wasn't found.");
@@ -76,16 +87,20 @@ public class MetroCommand implements CommandExecutor {
 	private class InvisItemTabComplete implements TabCompleter {
 		@SuppressWarnings("unchecked")
 		public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+			List<String> arguments = new ArrayList<String>();
 			if (args.length == 1) {
 				List<String> s = (List<String>) plugin.getConfig().getList("Stations");
 				List<String> sc = (List<String>) plugin.getConfig().getList("StationCodes");
-				List<String> arguments = new ArrayList<String>();
 				for(int i = 0; i < sc.size(); i++) {
 					arguments.add(sc.get(i));
 				}
 				for(int i = 0; i < s.size(); i++) {
 					arguments.add(s.get(i));
 				}
+				arguments.add("list");
+				return arguments;
+			} else if(args.length == 2) {
+				arguments.add("go away");
 				return arguments;
 			} else {
 				return null;
